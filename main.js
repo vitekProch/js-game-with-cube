954c7a065676d991bb0cf75558d9fafb646e369d
Vitalij Prochazka <vitek.prochazka1@seznam.cz>
2023-02-10 19:18:49 +0100

    Create EventListener newGame

--- main.js
+++ main.js
@@ -4,24 +4,6 @@ let cubeNumbers = ["one" ,"two" ,"three" ,"four" ,"five" ,"six"];
 let actualPlayer = 0;
 let winScore = 10;
 
-function cleanStart(){
-    finalScore = [0,0];
-    roundScore = 0;
-    actualPlayer = 0;
-
-    document.querySelector(".score-player-0").textContent = "0";
-    document.querySelector(".score-player-1").textContent = "0";
-
-    document.querySelector(".current-score-player-0").textContent = "0";
-    document.querySelector(".current-score-player-1").textContent = "0";
-
-    document.querySelector(".score-box-0").classList.add("active");
-    document.querySelector(".score-box-1").classList.remove("active");
-
-    document.querySelector(".player-name-0").textContent = "Skóre 1. hráče";
-    document.querySelector(".player-name-1").textContent = "Skóre 2. hráče";
-}
-
 // ----------- ROLL CUBE AND CURRENT SCORE UPDATE -----------
 document.getElementById("rollCube").addEventListener("click", function (){
    let cube = document.getElementById("cube");
@@ -51,6 +33,26 @@ document.getElementById("saveScore").addEventListener("click", function (){
     else{
         nextPlayer();
     }
+});
+document.getElementById("newGame").addEventListener("click", function (){
+    finalScore = [0,0];
+    roundScore = 0;
+    actualPlayer = 0;
+
+    document.querySelector(".score-player-0").textContent = "0";
+    document.querySelector(".score-player-1").textContent = "0";
+
+    document.querySelector(".current-score-player-0").textContent = "0";
+    document.querySelector(".current-score-player-1").textContent = "0";
+
+    document.querySelector(".score-box-0").classList.add("active");
+    document.querySelector(".score-box-1").classList.remove("active");
+
+    document.querySelector(".player-name-0").textContent = "Skóre 1. hráče";
+    document.querySelector(".player-name-1").textContent = "Skóre 2. hráče";
+
+    document.getElementById("rollCube").classList.remove("disabled");
+    document.getElementById("saveScore").classList.remove("disabled");
 });
 
 function winGame(){
