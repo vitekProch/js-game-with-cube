9a352bc4e463ce11cbe7e99c7b4db8ff7364786e
Vitalij Prochazka <vitek.prochazka1@seznam.cz>
2023-02-10 18:54:39 +0100

    Create function nextPlayer

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let finalScore = [0,0];
 let roundScore = 0;
 let cubeNumbers = ["one" ,"two" ,"three" ,"four" ,"five" ,"six"];
+let actualPlayer = 0;
 
 function cleanStart(){
     finalScore = [0,0];
     roundScore = 0;
+    actualPlayer = 0;
 
     document.querySelector(".score-player-0").textContent = "0";
     document.querySelector(".score-player-1").textContent = "0";
@@ -30,4 +32,27 @@ document.getElementById("rollCube").addEventListener("click", function (){
        cube.children[i].classList.remove(actualNumber);
        cube.children[i].classList.add(cubeNumbers[randomNumber]);
    }
+   if (randomNumber !== 0){
+       roundScore += randomNumber + 1;
+       document.querySelector(".current-score-player-" + actualPlayer).textContent = roundScore;
+   }
+   else {
+       nextPlayer();
+   }
 });
+
+function nextPlayer(){
+    roundScore = 0;
+    document.querySelector(".current-score-player-" + actualPlayer).textContent = roundScore;
+
+    document.querySelector(".score-box-0").classList.toggle("active");
+    document.querySelector(".score-box-1").classList.toggle("active");
+
+    if (actualPlayer === 0){
+        actualPlayer = 1;
+    }
+    else{
+        actualPlayer = 0;
+    }
+
+}
