47539a66332763c850a51da71d9a5b9491a05474
Vitalij Prochazka <vitek.prochazka1@seznam.cz>
2023-02-10 18:42:07 +0100

    Ctreate function roll cube

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let finalScore = [0,0];
 let roundScore = 0;
+let cubeNumbers = ["one" ,"two" ,"three" ,"four" ,"five" ,"six"];
 
 function cleanStart(){
     finalScore = [0,0];
@@ -17,3 +18,16 @@ function cleanStart(){
     document.querySelector(".player-name-0").textContent = "Skóre 1. hráče";
     document.querySelector(".player-name-1").textContent = "Skóre 2. hráče";
 }
+
+// ----------- ROLL CUBE -----------
+document.getElementById("rollCube").addEventListener("click", function (){
+   let cube = document.getElementById("cube");
+   let randomNumber = Math.floor(Math.random()*cubeNumbers.length);
+   let actualNumber = cube.children[0].classList[1];
+
+    // Getting cube dot and add class by random number.
+   for(let i = 0; i < cube.children.length; i++){
+       cube.children[i].classList.remove(actualNumber);
+       cube.children[i].classList.add(cubeNumbers[randomNumber]);
+   }
+});
